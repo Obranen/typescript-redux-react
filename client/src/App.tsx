c16dc52bc72a8366205f671f4f4b7da912297d28
Obranen <krakhnytskyi@gmail.com>
2022-06-09 13:54:41 +0300

    added reducer, action and others components project related

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import UserList from './components/UserList/UserList';
 
 function App() {
   return (
     <>
-      <h1>Hello!</h1>
+      <UserList/>
     </>
   );
 }
---
+++ client/src/components/UserList/UserList.tsx
@@ -0,0 +1,29 @@
+import React, {FC, useEffect} from 'react'
+import {useTypedSelector} from '../../hooks/useTypedSelector';
+import {fetchUsers} from '../../store/action-creators/user';
+import {useActions} from '../../hooks/useActions';
+
+const UserList: FC = () => {
+    const {users, loading, error} = useTypedSelector(state => state.user)
+    const {fetchUsers} = useActions()
+
+    useEffect(() => {
+      fetchUsers()
+    }, [])
+
+    if (loading) {
+      return <h1>Идёт загрузка...</h1>
+    }
+    if (error) {
+      return <h1>{error}</h1>
+    }
+    return (
+      <>
+        {users.map(user =>
+          <div key={user.id}>{user.name}</div>
+        )}
+      </>
+    )
+  }
+
+export default UserList
